d5579c8fb9b687d3f05f623e347277e2b09db0b2
gabrielk99 <gabriel_100@hotmail.com>
2021-09-10 01:31:17 -0300

    nothing

--- CP3/casosSucesso/teste.dart
+++ CP3/casosSucesso/teste.dart
@@ -18,34 +18,14 @@ void main(){
 int k = 2;
 
 void main(){
-    // bool v = true || true & false;
-    // if(v && true && true) {
-    //     print("to no then");
-    // }else {
-    //     print("to no else");
+    int x=1;
+    if(true){
+        //x = 4+2;
+       // print(x);
+    }
+    // else {
+    //     int y = 4+3;
+    //     print(y);
     // }
-
-    // if(!v)
-    //     print("esse nao tem else");
-
-    // if(v && false)
-    //     print("ih nesse then eu nao entro");
-    // else{
-    //     print("opa, esse entrei no else");
-    // }
-
-    // List <int> x = [12,3]+[32];
-
-    // print(x);
-    bool t = 3<4;
-    print("a"!="b");
-    print("af"=="");
-    print([12,3]!=[12,3]);
-    //print(3<4);
-    // print(4>3);
-    //print(false);
-    // List <double> y = [4.2];
-    // List <double> z = [1.3,12.4];
-    // print(y+z+[13.13,312.3,1.24]);
-    // //print([]+y+z+[4.5,1.3,0.1241,14.1]); corrigir
+    
 }
