d22e9154e5488ac765d6608875a967f96aea4a0c
mikaellafs <mikaellaferreira0@gmail.com>
2021-09-11 21:22:34 -0300

    for e atribuicao de lista

--- CP3/casosSucesso/teste.dart
+++ CP3/casosSucesso/teste.dart
@@ -2,13 +2,25 @@
 int k = 2;
 
 void main(){
-    List <int> x =[1,2];
+    /*List <int> x =[1,2];
     List <double> v = [1.3,1.4];
     List <String> z = ["da","da"];
     List <bool> c = [true,false];
 
    if(c[0]||c[1]){
        print(x[1]+v[0]*v[1]-x[0]);
-   }
+   }*/
+
+  List <int> v = [1,2,3,5];
+  int j = 2;
+  j = 15;
+  v[3] = j;
+  print(v);
+
+  /*
+  int i = 0;
+  for (; ; i = i+1){
+    print(i);
+  }*/
 }
 
